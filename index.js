f40a96469a5ec8e388c3fd7367be7f880cc8b0ec
BenjaminLHONNEN <benjaminlhonnen@gmail.com>
2018-05-14 10:22:34 +0200

    opti

--- index.js
+++ index.js
@@ -3,25 +3,15 @@ const fs = require('fs');
 const fileName = "test";
 
 function saveFile() {
-    if (fs.existsSync(fileName)) {
-        fs.appendFile(fileName, "\n" + Date.now(), function (err) {
-            if(err) {
-                console.log("Erreur : ");
-                return console.log(err);
-            }
-            console.log("Fichier sauvegarde !");
-        });
-    } else {
-        fs.writeFile(fileName, Date.now(), function(err) {
-            if(err) {
-                console.log("Erreur : ");
-                return console.log(err);
-            }
-            console.log("Creation du fichier !");
-        });
-    }
+    fs.appendFile(fileName, "\n" + Date.now(), function (err) {
+        if (err) {
+            console.log("Erreur : ");
+            return console.log(err);
+        }
+        console.log("Fichier sauvegarde !");
+    });
 }
 
 setInterval(function () {
     saveFile()
-},1000);
+}, 1000);
