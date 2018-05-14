1b02a88dd7e66a73bfe39c3b417cb137c33eed0d
BenjaminLHONNEN <benjaminlhonnen@gmail.com>
2018-05-14 09:48:28 +0200

    optimisation code

--- index.js
+++ index.js
@@ -4,21 +4,12 @@ const fileName = "test";
 
 function saveFile() {
     if (fs.existsSync(fileName)) {
-        fs.readFile(fileName, 'utf8', function (err,data) {
-            if (err) {
+        fs.appendFile(fileName, "\n" + Date.now(), function (err) {
+            if(err) {
                 console.log("Erreur : ");
                 return console.log(err);
             }
-            console.log("Contenu du fichier : ");
-            console.log(data);
-            let file = data + "\n" + Date.now();
-            fs.writeFile(fileName, file, function(err) {
-                if(err) {
-                    console.log("Erreur : ");
-                    return console.log(err);
-                }
-                console.log("Fichier sauvegarde !");
-            });
+            console.log("Fichier sauvegarde !");
         });
     } else {
         fs.writeFile(fileName, Date.now(), function(err) {
