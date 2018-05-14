b25f10b898da7a12026a26743b14ea473a0c42cb
BenjaminLHONNEN <benjaminlhonnen@gmail.com>
2018-05-14 09:33:53 +0200

    correction bug erreur si fichier innexistant

--- index.js
+++ index.js
@@ -3,22 +3,32 @@ const fs = require('fs');
 const fileName = "test";
 
 function saveFile() {
-    fs.readFile(fileName, 'utf8', function (err,data) {
-        if (err) {
-            console.log("Erreur : ");
-            return console.log(err);
-        }
-        console.log("Contenu du fichier : ");
-        console.log(data);
-        let file = data + "\n" + Date.now();
-        fs.writeFile(fileName, file, function(err) {
+    if (fs.existsSync(fileName)) {
+        fs.readFile(fileName, 'utf8', function (err,data) {
+            if (err) {
+                console.log("Erreur : ");
+                return console.log(err);
+            }
+            console.log("Contenu du fichier : ");
+            console.log(data);
+            let file = data + "\n" + Date.now();
+            fs.writeFile(fileName, file, function(err) {
+                if(err) {
+                    console.log("Erreur : ");
+                    return console.log(err);
+                }
+                console.log("Fichier sauvegarde !");
+            });
+        });
+    } else {
+        fs.writeFile(fileName, Date.now(), function(err) {
             if(err) {
                 console.log("Erreur : ");
                 return console.log(err);
             }
-            console.log("Fichier sauvegarde !");
+            console.log("Creation du fichier !");
         });
-    });
+    }
 }
 
 setInterval(function () {
